8a1808999fb5ae63765154444ddbd79f98776bbd
teabyii <teabyii@gmail.com>
2018-11-07 18:12:56 +0800

    chore: add react-native-video for example

--- example/App.js
+++ example/App.js
@@ -6,24 +6,22 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from 'react';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
+import Video from 'react-native-video';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Video 
+            style={styles.video}
+            source={require('./broadchurch.mp4')} 
+            controls
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -31,18 +29,12 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  video: {
+    marginTop: 50,
+    width: 375,
+    height: 250
+  }
 });
